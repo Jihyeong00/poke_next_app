ed4a54a17799082b5c21d5c70bbce6ebb6d38044
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2023-10-26 18:25:24 +0900

    📝fix : 전역변수 제거 버그수정

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -1,18 +1,11 @@
-'use client';
 import React from 'react';
 import Image from 'next/image';
 import pokeLogo from '@/assets/png/PokéLogo.png';
 import Link from 'next/link';
-import { thisHref } from '@/store/category';
-import { useSetAtom } from 'jotai/react';
 
 const HeaderLogo = () => {
-  const setState = useSetAtom(thisHref);
-  const linkMainPageAction = () => {
-    setState('/main');
-  };
   return (
-    <Link onClick={linkMainPageAction} className={'Logo'} href={'/'}>
+    <Link className={'Logo'} href={'/'}>
       <Image className={'h-12 w-40'} src={pokeLogo} alt={'배경이미지'} />
     </Link>
   );
